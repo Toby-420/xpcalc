d3008e774e73bbfb30986f7a10ff4f17bee431ed
Toby <83547635+Toby-420@users.noreply.github.com>
2022-10-30 22:49:24 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "xpcalc";
+            Console.Write("wanderer\n");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Console.Write("adventurer\n");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("hero\n");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(w * 1000 + a * 5000 + h * 20000 + "xp");
+            Console.Read();
+        }
+    }
+}
