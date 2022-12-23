4bb30c7454fa5ab20a03da71cf54b7dc5934c726
Toby-420 <83547635+Toby-420@users.noreply.github.com>
2022-12-23 19:53:26 +0000

    restructure

---
+++ c# version/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "xpcalc";
+            Console.Write("wanderer\n");
+            decimal w = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("adventurer\n");
+            decimal a = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("hero\n");
+            decimal h = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(w * 1000 + a * 5000 + h * 20000 + "xp");
+            Console.Read();
+        }
+    }
+}
